2d181c035e8079903bf64891a035e1656ef1fb13
SEMIN-97 <jsemin.1021@gmail.com>
2024-07-21 12:31:00 +0900

    Style: Lint 규칙 추가 및 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,41 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    "plugin:react/recommended",
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
+  settings: {
+    'react': {
+      'version': 'detect',
+    },
+  },
   rules: {
+    'no-var': 'error',
+    'semi': ['error', 'always'],
+    'quotes': [
+      'error',
+      'single',
+      {
+        'allowTemplateLiterals': true
+      }
+    ],
+    'comma-dangle': ['error', 'never'],
+    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 1, 'maxBOF': 1 }],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-tag-spacing': [
+      'error',
+      {
+        'closingSlash': 'never',
+        'beforeSelfClosing': 'always',
+        'afterOpening': 'never'
+      }
     ],
   },
 }
