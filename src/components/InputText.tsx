fbf3ccea705bcd588ba919a41350c7c8059df906
SEMIN-97 <jsemin.1021@gmail.com>
2024-08-12 22:31:31 +0900

    Feat: InputText 컴포넌트 생성

---
+++ src/components/InputText.tsx
@@ -0,0 +1,42 @@
+import { ChangeEvent } from 'react';
+import clsx from 'clsx';
+
+import styles from './InputText.module.scss';
+
+interface InputTextProps {
+  label: string;
+  value: string;
+  placeholder?: string;
+  disabled?: boolean;
+  errorMessage?: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function InputText({
+  label,
+  value,
+  placeholder,
+  disabled,
+  errorMessage,
+  onChange
+}: InputTextProps) {
+
+  return (
+    <div className={clsx(
+      styles.inputWrap,
+      {
+        [styles.error]: errorMessage
+      }
+    )}>
+      <label>{ label }</label>
+      <input
+        type="text"
+        value={value}
+        placeholder={placeholder}
+        disabled={disabled}
+        onChange={onChange}
+      />
+      <p className={styles.errorMessage}>{ errorMessage }</p>
+    </div>
+  );
+}
