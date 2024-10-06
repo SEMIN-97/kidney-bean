eee9a52a0aba0759465ce2f55b578c5c965ace9c
SEMIN-97 <jsemin.1021@gmail.com>
2024-10-06 15:32:21 +0900

    build: tanstack/reactQuery, 관련 린트 설치, 초기 설정
    * Lint 설정
    * QueryClientProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import SignInPage from './pages/signIn';
 import styles from './App.module.scss';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <main className={styles.app}>
-      <SignInPage />
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main className={styles.app}>
+        <SignInPage />
+      </main>
+    </QueryClientProvider>
   );
 }
 
