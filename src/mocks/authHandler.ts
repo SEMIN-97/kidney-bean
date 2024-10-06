f98416c670d9d2d4fab5a16b13232c4c863838b6
SEMIN-97 <jsemin.1021@gmail.com>
2024-10-06 13:33:57 +0900

    feat: 로그인, 회원가입 목업 API 작성

--- src/mocks/authHandler.ts
+++ src/mocks/authHandler.ts
@@ -1,6 +1,6 @@
 import { http, HttpResponse } from 'msw';
 
-import { LoginRequest, ValidateEmailPathParams } from '../types/authType.ts';
+import { LoginRequest, User, ValidateEmailPathParams } from '../types/authType.ts';
 
 const endPoint = '/api/auth';
 const verifiedUser: LoginRequest  = {
@@ -17,5 +17,23 @@ export const authHandler = [
     }
 
     return new HttpResponse(null, { status: 400 });
+  }),
+  http.post<never, LoginRequest>(`${endPoint}/login`, async ({ request }) => {
+    const { email, password } = await request.json();
+
+    if (email === verifiedUser.email && password === verifiedUser.password) {
+      return new HttpResponse(null, { status: 200 });
+    }
+
+    return new HttpResponse(null, { status: 401 });
+  }),
+  http.post<never, User, User>(`${endPoint}/user`, async ({ request }) => {
+    const { email, nickname } = await request.json();
+
+    if (email === verifiedUser.email) {
+      return HttpResponse.json(null, { status: 409 });
+    }
+
+    return HttpResponse.json({ email, nickname }, { status: 201 });
   })
 ];
--- src/types/authType.ts
+++ src/types/authType.ts
@@ -6,3 +6,9 @@ export interface LoginRequest {
   email: string;
   password: string;
 }
+
+export interface User {
+  email: string;
+  password?: string;
+  nickname: string;
+}
